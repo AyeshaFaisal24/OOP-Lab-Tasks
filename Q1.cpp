381364ca93e08f2ab2d34f9302e739a3a6e31f16
AyeshaFaisal24 <ayeshafaisal328@gmail.com>
2025-01-31 10:00:20 +0500

    Add files via upload

--- Q1.cpp
+++ Q1.cpp
@@ -1,22 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main (int argc , char *argv[]) {
-	float max=0 , sec_max=0 ;
-	
-	for (int i=1 ; i<argc ; i++) {
-		if (stof(argv[i]) > max) {
-			sec_max = max ;
-			max = stof(argv[i]);
-		}
-		
-		if (stof(argv[i]) !=  max && stof(argv[i]) > sec_max){
-		 	sec_max = stof(argv[i]);
-		 }
-	}
-	
-	cout << "Second Highest:" << sec_max;
-	
-	return 0;
-}
+    int sum=0 , *ptr;
 
+
+    for (int i=1 ; i<argc ; i++) {
+        int num = stoi(argv[i]);
+        ptr = &num;
+        sum += *ptr;
+    }
+
+    cout << "Sum is:" << sum ;
+
+    return 0;
+}
